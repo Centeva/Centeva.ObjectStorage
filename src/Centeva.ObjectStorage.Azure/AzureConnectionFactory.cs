ffc64574b39d2af20e818d23958d060cd84b3f95
Robert Emmett <robert.emmett@centeva.com>
2023-09-11 12:27:48 -0600

    Assumes default {name}.blob.core.windows.net unless Endpoint present in connection string
    This will simplify the connection string and factory requirements for Azure object storage. Also brings more inline with S3 implementation.

--- src/Centeva.ObjectStorage.Azure/AzureConnectionFactory.cs
+++ src/Centeva.ObjectStorage.Azure/AzureConnectionFactory.cs
@@ -5,23 +5,19 @@ namespace Centeva.ObjectStorage.Azure;
 public class AzureConnectionFactory : IConnectionFactory
 {
     private const string ProviderName = "azure";
-    private const string Protocol = "DefaultEndpointsProtocol";
-    private const string EndpointSuffix = "EndpointSuffix";
     private const string AccountName = "AccountName";
     private const string AccountKey = "AccountKey";
+    private const string Endpoint = "Endpoint";
 
     public IObjectStorage? CreateConnection(ObjectStorageConnectionString connectionString)
     {
         if (connectionString.ProviderName != ProviderName)
             return null;
 
-        // The connection string contains the parts that make up the URI endpoint
-        // {DefaultEndpointsProtocol}://{AccountName}.blob.{EndpointSuffix}
-        var protocol = connectionString.Get(Protocol) ?? "https";
-        var suffix = connectionString.Get(EndpointSuffix) ?? "core.windows.net";
+        const string suffix = "core.windows.net";
         var accountName = connectionString.GetRequired(AccountName);
         var accountKey = connectionString.GetRequired(AccountKey).Replace(' ', '+');
-        var endpoint = new Uri($"{protocol}://{accountName}.blob.{suffix}");
+        var endpoint = new Uri(connectionString.Get(Endpoint) ?? $"https://{accountName}.blob.{suffix}");
 
         return new AzureObjectStorage(accountName, accountKey, endpoint);
     }
