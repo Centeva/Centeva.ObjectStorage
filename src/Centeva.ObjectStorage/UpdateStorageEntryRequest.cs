e05829d8e096742dd4cc0fd2e25f6318eccb8b20
devyn (devcon) cyphers <devyncyphers@gmail.com>
2024-11-12 12:09:47 -0700

    Fix declarations

--- src/Centeva.ObjectStorage/UpdateStorageEntryRequest.cs
+++ src/Centeva.ObjectStorage/UpdateStorageEntryRequest.cs
@@ -2,5 +2,5 @@
 
 public class UpdateStorageEntryRequest
 {
-    public Dictionary<string, string> Metadata { get; set; }
+    public Dictionary<string, string> Metadata { get; set; } = new Dictionary<string, string>();
 }
