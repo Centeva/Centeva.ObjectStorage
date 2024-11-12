5e40497d70fc72aeaf9f80deafa5dd59438d093b
devyn (devcon) cyphers <devyncyphers@gmail.com>
2024-11-12 12:07:14 -0700

    Pull out ISupportsMetadata from IObjectStorage

--- src/Centeva.ObjectStorage/ISupportsMetadata.cs
+++ src/Centeva.ObjectStorage/ISupportsMetadata.cs
@@ -5,4 +5,21 @@
 /// </summary>
 public interface ISupportsMetadata
 {
+
+    /// <summary>
+    /// Update information about a stored object at the given path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="request"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task UpdateMetadataAsync(StoragePath path, UpdateStorageEntryRequest request, CancellationToken cancellationToken = default);
+
+    /// <summary>
+    /// Get metadata for a stored object at the given path
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<StorageEntry> GetMetadataAsync(StoragePath path, CancellationToken cancellationToken = default);
 }
