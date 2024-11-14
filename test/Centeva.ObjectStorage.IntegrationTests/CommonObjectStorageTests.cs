7a45612be0e332a5ba8713129786431ba7cbab76
Devyn Cyphers <devyn.cyphers@centeva.com>
2024-11-14 15:56:42 -0700

    Enable long running test

--- test/Centeva.ObjectStorage.IntegrationTests/CommonObjectStorageTests.cs
+++ test/Centeva.ObjectStorage.IntegrationTests/CommonObjectStorageTests.cs
@@ -202,7 +202,7 @@ public abstract class CommonObjectStorageTests
         entry.SizeInBytes.Should().Be(_testFileContent.Length);
     }
 
-    [Fact(Skip = "I hate waiting")]
+    [Fact]
     public async Task ListAsync_LotsOfFiles()
     {
         var currentCount = (await _sut.ListAsync(_storagePathPrefix)).Count;
