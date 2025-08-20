84d6fe93c0e4c2edfdddb9e156ab81c8900274a6
Pete Deffendol <pete.deffendol@centeva.com>
2025-08-20 07:49:34 -0600

    Add ability to set Content-Disposition header for signed URLs
    This allows setting the header only upon requesting a signed URL for a
    stored object, instead of having to specify it when uploading.
    
    I removed the old method signature as it was causing issues with the
    ambiguous parameter list.

--- src/Centeva.ObjectStorage/ISupportsSignedUrls.cs
+++ src/Centeva.ObjectStorage/ISupportsSignedUrls.cs
@@ -13,14 +13,4 @@ public interface ISupportsSignedUrls
     /// <param name="cancellationToken"></param>
     /// <returns></returns>
     Task<Uri> GetDownloadUrlAsync(StoragePath path, SignedUrlOptions? options = null, CancellationToken cancellationToken = default);
-
-    /// <summary>
-    /// (deprecated) Gets a signed URL for retrieving an object at the given path
-    /// </summary>
-    /// <remarks>This version of the method is deprecated in favor of passing <see cref="SignedUrlOptions"/></remarks>
-    /// <param name="path"></param>
-    /// <param name="lifetimeInSeconds"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
-    Task<Uri> GetDownloadUrlAsync(StoragePath path, int lifetimeInSeconds = 86400, CancellationToken cancellationToken = default);
 }
--- src/Centeva.ObjectStorage/SignedUrlOptions.cs
+++ src/Centeva.ObjectStorage/SignedUrlOptions.cs
@@ -1,4 +1,6 @@
-﻿namespace Centeva.ObjectStorage;
+﻿using System.Net.Mime;
+
+namespace Centeva.ObjectStorage;
 
 /// <summary>
 /// Options for producing signed URLs
@@ -9,4 +11,9 @@ public record struct SignedUrlOptions()
     /// How long the signed URL should be active
     /// </summary>
     public TimeSpan Duration { get; set; } = TimeSpan.FromDays(1);
+
+    /// <summary>
+    /// Value of the Content-Disposition header sent when the signed URL is used to download the content.
+    /// </summary>
+    public ContentDisposition? ContentDisposition { get; set; }
 }
