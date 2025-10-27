cf978d97c888deb893612f4e29fce121b66dde73
Robert Emmett <robert.emmett@centeva.com>
2025-10-27 16:09:20 -0600

    Adds extension methods for CopyAsync and CopyAllSync
    - CopyAsync will copy an object from the source provider container to a target provider (same or different) container
    - CopyAllAsync will recursively copy all objects from the source provider container to a target provider (same or diffent) container

---
+++ src/Centeva.ObjectStorage/Extensions/ObjectStorageExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace Centeva.ObjectStorage.Extensions;
+
+public static class ObjectStorageExtensions
+{
+    public static async Task CopyAsync(this IObjectStorage sourceStorage, StoragePath sourcePath, IObjectStorage targetStorage, StoragePath? targetPath, CancellationToken cancellationToken = default)
+    {
+        targetPath = targetPath?.IsFolder == true
+            ? new StoragePath(StoragePath.Combine(targetPath.Full, sourcePath.Name))
+            : targetPath ?? sourcePath;
+
+        using var sourceStream = await sourceStorage.OpenReadAsync(sourcePath, cancellationToken);
+        await targetStorage.WriteAsync(targetPath, sourceStream!, cancellationToken: cancellationToken);
+    }
+
+    public static async Task CopyAllAsync(this IObjectStorage sourceStorage, StoragePath sourcePath, IObjectStorage targetStorage, StoragePath? targetPath, CancellationToken cancellationToken = default)
+    {
+        var entries = await sourceStorage.ListAsync(sourcePath, new ListOptions { Recurse = true }, cancellationToken);
+        var entryPaths = entries.Select(x => x.Path).ToList();
+
+        foreach (var entryPath in entryPaths)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var relativePath = entryPath.Full.Substring(sourcePath.Full.Length).TrimStart(StoragePath.PathSeparator);
+            var targetEntryPath = targetPath?.IsFolder == true
+                ? new StoragePath(StoragePath.Combine(targetPath.Full, relativePath))
+                : new StoragePath(relativePath);
+
+            if (entryPath.IsFolder)
+                continue;
+
+            using var sourceStream = await sourceStorage.OpenReadAsync(entryPath, cancellationToken)
+                ?? throw new IOException($"Source file not found: {entryPath}");
+
+            await targetStorage.WriteAsync(targetEntryPath, sourceStream, cancellationToken: cancellationToken);
+        }
+    }
+}
