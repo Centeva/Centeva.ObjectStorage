f303252bcd6fc9cad4d6c1aea7a4ca1034baebe2
Robert Emmett <robert.emmett@centeva.com>
2025-10-28 12:57:32 -0600

    Filters out Folders rather than including and skipping over them when recursively copying objects; uses a new variable name (destPath) rather than modifying incoming parameter

--- src/Centeva.ObjectStorage/Extensions/ObjectStorageExtensions.cs
+++ src/Centeva.ObjectStorage/Extensions/ObjectStorageExtensions.cs
@@ -4,25 +4,22 @@ public static class ObjectStorageExtensions
 {
     public static async Task CopyAsync(this IObjectStorage sourceStorage, StoragePath sourcePath, IObjectStorage targetStorage, StoragePath? targetPath, CancellationToken cancellationToken = default)
     {
-        targetPath = targetPath?.IsFolder == true
+        var destPath = targetPath?.IsFolder == true
             ? new StoragePath(StoragePath.Combine(targetPath.Full, sourcePath.Name))
             : targetPath ?? sourcePath;
 
         using var sourceStream = await sourceStorage.OpenReadAsync(sourcePath, cancellationToken);
-        await targetStorage.WriteAsync(targetPath, sourceStream!, cancellationToken: cancellationToken);
+        await targetStorage.WriteAsync(destPath, sourceStream!, cancellationToken: cancellationToken);
     }
 
     public static async Task CopyAllAsync(this IObjectStorage sourceStorage, StoragePath sourcePath, IObjectStorage targetStorage, StoragePath? targetPath, CancellationToken cancellationToken = default)
     {
         var entries = await sourceStorage.ListAsync(sourcePath, new ListOptions { Recurse = true }, cancellationToken);
-        var entryPaths = entries.Select(x => x.Path).ToList();
+        var entryPaths = entries.Where(x => !x.Path.IsFolder).Select(x => x.Path).ToList();
 
         foreach (var entryPath in entryPaths)
         {
             cancellationToken.ThrowIfCancellationRequested();
-
-            if (entryPath.IsFolder)
-                continue;
 
             var relativePath = entryPath.Full.Substring(sourcePath.Full.Length);
             var targetEntryPath = targetPath?.IsFolder == true
